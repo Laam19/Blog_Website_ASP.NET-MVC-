866cb3e6482a9621fc0405ef6046966eb94112a4
Laam19 <41922852+Laam19@users.noreply.github.com>
2020-08-19 22:52:24 +0600

    styling

--- Blog website Asp.Net/ViewModels/LogInViewModel.cs
+++ Blog website Asp.Net/ViewModels/LogInViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace Blog_website_Asp.Net.ViewModels
@@ -8,6 +10,7 @@ namespace Blog_website_Asp.Net.ViewModels
     public class LogInViewModel
     {
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
         public string password { get; set; }
     }
 }
